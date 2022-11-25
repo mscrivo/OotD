f156cd31f01294f11778ebe815efb26ca5259f32
Michael Scrivo <mscrivo@gmail.com>
2022-11-24 21:57:36 -0500

    Update code signing instructions

--- ServerStaging/signInstaller.bat
+++ ServerStaging/signInstaller.bat
@@ -1,5 +1,4 @@
 @echo off
-rem To use this, you must have the cert installed to your personal cert store and you have to launch SimplySign Desktop and login with a token from SimplySign Mobile.
 set /p path="Enter path to installer: "
-"C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe" sign /n "Open Source Developer, Michael Scrivo" /t http://timestamp.comodoca.com/authenticode /fd SHA256 %path%
+"C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f "ootd.pfx" %path%
 set /p throwaway=Hit ENTER to continue...
