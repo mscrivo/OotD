3bd1c018eaaf50100725614d55f1129b1e50c7b6
Michael Scrivo <mscrivo@gmail.com>
2023-04-01 14:21:24 -0400

    Remove unused file

--- ServerStaging/signInstaller.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p path="Enter path to installer: "
-"C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f "ootd.pfx" %path%
-set /p throwaway=Hit ENTER to continue...
